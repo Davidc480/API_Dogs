0dae5000da5ad965d20f82caafedae4bb12b4d9c
Davidc480 <Davidcalle.480@gmail.com>
2023-04-20 20:00:19 -0500

    Creando componente NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-import Home from "./views/Home/Home";
+import { Home, Landing, Form, Detail } from "./views";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
-      <Home></Home>
+      <Route exact path="/" render={() => <Landing />} />
+
+      <Route path="/Home" render={() => <Home />} />
+
+      <Route path="/create" render={() => <Form />} />
+
+      <Route path="/Detail" render={() => <Detail />} />
     </div>
   );
 }
